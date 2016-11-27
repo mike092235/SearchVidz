e821fd4ab5884c00414a2c8d21f1f31eb694a5fa
Michael Emmanuel <michael.emmanuel732@gmail.com>
2016-11-27 11:48:29 -0500

    search

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ $(function(){
   var searchField = $('#query');
   var icon = $('#search-btn');
 
-  //Focus Handler
-  $('searchField').on('focus', function(){
+  //Focus Event Handler
+  $(searchField).on('focus', function(){
     $(this).animate({
       width:'100%'
     },400);
@@ -12,4 +12,57 @@ $(function(){
       right: '10px'
     }, 400);
   });
+
+  // Blur Event Handler
+  $(searchField).on('blur', function(){
+    if(searchField.val() == ''){
+      $(searchField).animate({
+        width: '45%'
+      },400, function(){});
+      $(icon).animate({
+        right: '360px'
+      },400, function(){});
+    }
+  });
+
+  $('searchForm').submit(function(){
+    e.preventDefault();
+  });
 });
+
+function search(){
+  //clear Results
+  $('#results').html(''); 
+  $('#buttons').html(''); 
+
+  //Get Form Input
+  q = $('#query').val();
+
+  //Run GET Request on API
+  $.get(
+      "https//www.googleapis.com/youtube/v3/search", {
+        part: 'snippet, id',
+        q: q,
+        type: 'video',
+        key: 'AIzaSyCejuvKTqPNAgeSleP9b5PHmE6tWUqztEg'},
+        function(data){
+          var nextPageToken = data.nextPageToken;
+          var prevPageToken = data.prevPageToken;
+
+          //Log Data
+          console.log(data);
+
+          $.each(data.items, function(i, item){
+            // Get output
+            var output = getOutput(item);
+
+            // Display Results
+            $('#results').append(output);
+          });
+        }
+  );
+}
+
+
+
+
