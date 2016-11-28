5ab86810a5452e87a562d2bbf05f873e3e4c02a4
Michael Emmanuel <michael.emmanuel732@gmail.com>
2016-11-28 11:14:54 -0500

    complete

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ $(function() {
     }
   });
 
-  $('searchForm').submit(function(e) {
+  $('#search-form').submit(function(e) {
     e.preventDefault();
   });
 })
@@ -69,7 +69,50 @@ function search() {
   );
 }
 
-//prev button 
+//Next Page button 
+function nextPage() {
+    var token = $('#next-button').data('token');
+    var q = $('#next-button').data('query');
+    
+    //clear results
+    $('#results').html('');
+    $('#buttons').html('');
+    
+    //get form input 
+    q = $('#query').val();
+    
+    //run GET request on API
+    $.get(
+        "https://www.googleapis.com/youtube/v3/search", {
+            part: 'snippet, id',
+            q: q,
+            pageToken: token,
+            type: 'video',
+            key: 'AIzaSyBzWo6ID2vMhbG4MQ9FKs7ncvFBtSUc6D4'
+        }, function(data) {
+            var nextPageToken = data.nextPageToken;
+            var prevPageToken = data.prevPageToken;
+            
+            //log data
+            console.log(data);
+//            
+            $.each(data.items, function(i, item) {
+                //get output
+                var output = getOutput(item);
+                
+                //display results
+                $('#results').append(output);
+            });
+            
+            var buttons = getButtons(prevPageToken, nextPageToken);
+            
+            //display buttons
+            $('#buttons').append(buttons);
+        }
+    );
+}
+
+//Prev Page button 
 function prevPage() {
     var token = $('#prev-button').data('token');
     var q = $('#prev-button').data('query');
@@ -113,11 +156,12 @@ function prevPage() {
 }
 
 
+
 // Build Output
 function getOutput(item){
   var videoId = item.id.videoId;
   var title = item.snippet.title;
-  var discription = item.snippet.description;
+  var description = item.snippet.description;
   var thumb = item.snippet.thumbnails.high.url;
   var channelTitle = item.snippet.channelTitle;
   var videoDate = item.snippet.publishedAt;
@@ -128,7 +172,7 @@ function getOutput(item){
     '<img src="'+thumb+'">' +
     '</div>' +
     '<div class="list-right">' +
-    '<h3>'+title+'</h3>'
+    '<h3><a class="fancybox fancybox.iframe" href="http://www.youtube.com/embed/'+videoId+'">'+title+'</a></h3>' +
     '<small>By <span class="cTitle">'+channelTitle+'</span> on '+videoDate+'</small>' +
     '<p>'+description+'</p>' +
     '</div>' +
